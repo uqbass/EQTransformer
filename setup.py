1fa0abf0312d1fb0ac74d370e3a3ee0047e4678a
uqbass <58973227+uqbass@users.noreply.github.com>
2022-11-24 18:34:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,23 +15,15 @@ setup(
     license="MIT",
     packages=find_packages(),
     keywords='Seismology, Earthquakes Detection, P&S Picking, Deep Learning, Attention Mechanism',
-    install_requires=[
-	'pytest==7.1.2',
-	'numpy==1.22.4',     # appox version: numpy 1.19.x but at least 1.19.2
-	'keyring==23.7.0', 
-	'pkginfo==1.8.3',
-	'scipy==1.8.1',
-	'tensorflow-deps==2.9.0',
-	'tensorflow-estimator==2.9.0',
-	'tensorflow-macos==2.9.2',
-	'tensorflow~=2.5.0', # tensorflow <2.7.0 needs numpy <1.20.0
-	'keras==2.9.0', 
-	'matplotlib-base==3.5.2', 
-	'pandas==1.4.3',
-	'tqdm==4.64.0', 
-	'h5py==3.6.0', 
-	'obspy==1.3.0',
-	'jupyter==1.0.0'], 
+      install_requires=[
+        'pytest',
+        'keyring',        
+        'pkginfo',
+        'tensorflow',         
+        'pandas',
+        'tqdm',        
+        'h5py',       
+        'obspy'],
 
     python_requires='==3.10.5',
 )
